5117e5322a4063b22aba228b138560d819d3fdc1
MarufHasan-dev <170166811+MarufHasan-dev@users.noreply.github.com>
2025-05-22 21:51:15 +0600

    add responsive styles for the lg screen logos.jsx

--- src/components/sections/Logos.jsx
+++ src/components/sections/Logos.jsx
@@ -3,7 +3,7 @@ import { logos } from "../../utils/content";
 export default function Logos() {
   return (
     <section className="bg-gradient-to-top">
-      <div className="mx-auto max-w-[90rem] px-25 py-28 max-xl:px-16 max-xl:py-24">
+      <div className="mx-auto max-w-[90rem] px-25 py-28 max-xl:px-16 max-xl:py-24 max-lg:px-8">
         <p className="text-primary-50 text-xl/loose font-light m-auto mb-20 max-xl:mb-16 text-center max-xl:text-lg/loose">
           Organizations powered by <span className="font-bold">NoteFlow</span>
         </p>
@@ -13,7 +13,7 @@ export default function Logos() {
               return (
                 <li key={logo.id}>
                   <img
-                    className="h-10 max-xl:h-8"
+                    className="h-10 max-xl:h-8 max-lg:h-7"
                     src={logo.src}
                     alt={logo.alt}
                   />
@@ -26,7 +26,7 @@ export default function Logos() {
               return (
                 <li key={logo.id}>
                   <img
-                    className="h-10 max-xl:h-8"
+                    className="h-10 max-xl:h-8 max-lg:h-7"
                     src={logo.src}
                     alt={logo.alt}
                   />
--- src/components/sections/Reviews.jsx
+++ src/components/sections/Reviews.jsx
@@ -2,7 +2,7 @@ import { reviews } from "../../utils/content";
 
 export default function Reviews() {
   return (
-    <section className="mx-auto flex items-center gap-4 flex-wrap max-w-[90rem] px-25 max-xl:px-16 max-xl:gap-3">
+    <section className="mx-auto flex items-center gap-4 flex-wrap max-w-[90rem] px-25 max-xl:px-16 max-xl:gap-3 max-lg:px-8 max-lg:gap-2">
       <ul className="flex">
         {reviews.map((review) => {
           return (
@@ -17,7 +17,7 @@ export default function Reviews() {
           );
         })}
       </ul>
-      <p className="text-xl/loose font-light text-primary-100">
+      <p className="text-xl/loose font-light text-primary-100 max-lg:text-base/loose">
         Trusted by{" "}
         <span className="text-primary-500 font-bold tracking-tighter">
           12,653+
